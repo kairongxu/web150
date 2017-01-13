7b2b5662af5f89c9dd665e86a5f2378176cf2391
kairongxu <kairong.xu@seattlecolleges.edu>
2017-01-12 19:40:35 -0800

    assignment1

---
+++ 1_variables_expressions/script.js
@@ -0,0 +1,14 @@
+/*this is a comment*/
+
+var x = 5;
+var firstname = "<span class='name'>Kairong</span>";
+document.write("My name is" + firstname);
+
+var lastname = "<span class='name'>Xu</span>";
+document.write(lastname);
+
+var yearsofstudy = "<span class='yearofstudy'>1 year</span>";
+document.write("I have been at SCC for" + yearsofstudy);
+
+var goal = "<span class='goal'>become a web designer</span>";
+document.write("and plan to" + goal + "after completing my courses.");
