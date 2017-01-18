c6f727be7d1031e8e3e03baeafc2972e782a0fa1
kairongxu <xukairong1991@gmail.com>
2017-01-17 19:33:52 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/*this is a comment*/
+
+var x = 5;
+var firstname = "<span class='name'>Kairong</span>";
+
+var lastname = "<span class='name'>Xu</span>";
+
+var yearsofstudy = "<span class='yearofstudy'>1 year</span>";
+
+var goal = "<span class='goal'>become a web designer</span>";
